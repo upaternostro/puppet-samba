c58b3555386d2264c07ae0e70d6d9a52e3e60137
Jon Mosco <jonny.mosco@gmail.com>
2013-08-08 16:25:41 -0400

    params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,4 +5,15 @@
 # Samba parameters
 #
 class samba::params {
+  # Winbind
+  $workgroup          = ''
+  $passwd_server      = ''
+  $realm              = ''
+  $security           = ''
+  $idmap_uid          = ''
+  $idmap_gid          = ''
+  $seperator          = ''
+  $shell              = ''
+  $use_default_domain = ''
+  $offline_login      = ''
 }
--- manifests/winbind.pp
+++ manifests/winbind.pp
@@ -1,16 +1,16 @@
 # == Class: samba::winbind
 #
 class samba::winbind (
-  $workgroup          = '',
-  $passwd_server      = '',
-  $realm              = '',
-  $security           = '',
-  $idmap_uid          = '',
-  $idmap_gid          = '',
-  $seperator          = '',
-  $shell              = '',
-  $use_default_domain = '',
-  $offline_login      = '',
+  $workgroup          = $samba::params::workgroup,
+  $passwd_server      = $samba::params::passwd_server,
+  $realm              = $samba::params::realm,
+  $security           = $samba::params::security,
+  $idmap_uid          = $samba::params::idmap_uid,
+  $idmap_gid          = $samba::params::idmap_gid,
+  $seperator          = $samba::params::seperator,
+  $shell              = $samba::params::shell,
+  $use_default_domain = $samba::params::use_default_domain,
+  $offline_login      = $samba::params::offline_login,
 ) {
 
   case $::operatingsystem {
