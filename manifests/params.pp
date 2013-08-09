57d8199e02d3fdc87b9ab91541eb7dcfa6c2aa5d
Jon Mosco <jonny.mosco@gmail.com>
2013-08-09 15:45:47 -0400

    parameter cleanup

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,22 +5,6 @@
 # Samba parameters
 #
 class samba::params {
-  #default settings
-  $logdir            = ''
-
-  # shares
-
-  # Winbind
-  #$workgroup          = ''
-  #$passwd_server      = ''
-  #$realm              = ''
-  #$security           = ''
-  #$idmap_uid          = ''
-  #$idmap_gid          = ''
-  #$seperator          = ''
-  #$shell              = ''
-  #$use_default_domain = ''
-  #$offline_login      = ''
 
   case $::operatingsystem {
     centos, redhat: {
